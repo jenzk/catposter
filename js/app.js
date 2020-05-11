b51e58c955e85e076dc572ad0b2c18faecac684f
Alf <alf.g.jr@gmail.com>
2020-05-10 23:35:40 -0700

    More PDF stuff.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
     var ownerEmail = document.getElementById('input-owner-email');
 
     var finishPosterBtn = document.getElementById('finish-poster');
+    var nextBtn = document.getElementById('next-button');
 
     // Add event listeners.
     title.addEventListener('input', onPosterTitleUpdate);
@@ -18,16 +19,56 @@
     ownerEmail.addEventListener('input', onOwnerUpdate);
     finishPosterBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        console.log('clicked');
 
-        html2canvas(document.querySelector("#poster-preview"), {
-        }).then(canvas => {
-            document.body.appendChild(canvas)
-            var imgData = canvas.toDataURL('image/png');
-            var doc = new jsPDF('p', 'mm', 'a4');
-            doc.addImage(imgData, 'JPEG', 0, 0);
-            doc.save('generated.pdf');
+        var previewTitle = document.getElementById('preview-title').textContent;
+        var previewInfo = document.getElementById('preview-info').textContent;
+        var previewOwnerName = document.getElementById('preview-owner-name').textContent;
+        var previewOwnerPhone = document.getElementById('preview-owner-phone').textContent;
+        var previewOwnerEmail = document.getElementById('preview-owner-email').textContent;
+        var img = document.getElementById('preview-image').getElementsByTagName('img')[0].src;
+
+        var doc = new jsPDF({
+            format: 'letter'
         });
+
+        var docWidth = doc.internal.pageSize.getWidth();
+        var imageProps = doc.getImageProperties(img);
+        var width = imageProps.width;
+
+        console.log(docWidth, width);
+
+        // var height = (imageProps.height * width) / width;
+
+        // console.log(imageProps);
+        // console.log(width, height);
+
+        doc.setFontSize(40);
+        doc.text(previewTitle, 105, 25, { align: 'center' })
+
+        doc.addImage(img, "JPEG", (docWidth - 150) / 2, 40, 150, 100);
+
+        doc.setFontSize(12);
+        doc.text(previewInfo, 105, 150, { align: 'center', maxWidth: 180 });
+
+        doc.setFontSize(20);
+        doc.text("Please Contact:", 105, 175, { align: 'center', maxWidth: 180 });
+
+        doc.setFontSize(12);
+        doc.text(previewOwnerName, 105, 180, { align: 'center', maxWidth: 180 });
+        doc.text(previewOwnerPhone, 105, 185, { align: 'center', maxWidth: 180 });
+        doc.text(previewOwnerEmail, 105, 190, { align: 'center', maxWidth: 180 });
+
+        doc.text("testing1", 105, 275, { align: 'center', maxWidth: 180, angle: 90, rotationDirection: 1 });
+        doc.text("testing2", 110, 275, { align: 'center', maxWidth: 180, angle: 90, rotationDirection: 1 });
+        doc.text("testing3", 115, 275, { align: 'center', maxWidth: 180, angle: 90, rotationDirection: 1 });
+        doc.text("testing4", 120, 275, { align: 'center', maxWidth: 180, angle: 90, rotationDirection: 1 });
+        // doc.autoPrint();
+        doc.save('poster.pdf');
+    });
+    nextBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        var posterPreview = document.getElementById('poster-preview'); 
+        posterPreview.classList.toggle('full-screen');
     });
 
     // Update all elements.
@@ -54,10 +95,13 @@ function updateAll() {
     var ownerName = document.getElementById('input-owner-name').value;
     var ownerPhone = document.getElementById('input-owner-phone').value;
     var ownerEmail = document.getElementById('input-owner-email').value;
-    var text = ownerName + " at " + ownerPhone + " or " + ownerEmail;
 
-    var previewOwnerInfo = document.getElementById('preview-owner-info');
-    previewOwnerInfo.textContent = text;
+    var previewOwnerName = document.getElementById('preview-owner-name');
+    previewOwnerName.textContent = ownerName;
+    var previewOwnerPhone = document.getElementById('preview-owner-phone');
+    previewOwnerPhone.textContent = ownerPhone;
+    var previewOwnerEmail = document.getElementById('preview-owner-email');
+    previewOwnerEmail.textContent = ownerEmail;
 }
 
 function onPosterTitleUpdate(e) {
@@ -76,10 +120,13 @@ function onOwnerUpdate(e) {
     var ownerName = document.getElementById('input-owner-name').value;
     var ownerPhone = document.getElementById('input-owner-phone').value;
     var ownerEmail = document.getElementById('input-owner-email').value;
-    var text = ownerName + " at " + ownerPhone + " or " + ownerEmail;
 
-    var previewOwnerInfo = document.getElementById('preview-owner-info');
-    previewOwnerInfo.textContent = text;
+    var previewOwnerName = document.getElementById('preview-owner-name');
+    previewOwnerName.textContent = ownerName;
+    var previewOwnerPhone = document.getElementById('preview-owner-phone');
+    previewOwnerPhone.textContent = ownerPhone;
+    var previewOwnerEmail = document.getElementById('preview-owner-email');
+    previewOwnerEmail.textContent = ownerEmail;
 }
 
 function dropZoneListener(drop) {
@@ -107,8 +154,8 @@ function dropZoneListener(drop) {
                 drop.appendChild(imgEl);
 
                 drop.classList.remove('border');
-                var posterPreview = document.getElementById('poster-preview');
-                posterPreview.classList.remove('border');
+                // var posterPreview = document.getElementById('poster-preview');
+                // posterPreview.classList.remove('border');
             });
         }
     });
